26fdc1c26fd4274a31a5dc202aa909b2b3786c76
MD AWAIZ <awaizkhanmd@gmail.com>
2023-02-07 00:54:23 +0530

    add additional files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { Routes, BrowserRouter, Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components/Index';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css'
 const App = () => {
   const activeMenu = false;
